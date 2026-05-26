1353a98b54313b26af6bdca84d6a7fd37e7bb59e
IGdotnet <belonfranzolinigor@gmail.com>
2026-05-26 20:30:00 -0300

    Upload component

--- refund-web/src/pages/Refund.tsx
+++ refund-web/src/pages/Refund.tsx
@@ -4,6 +4,7 @@ import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
 
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
+import { Upload } from "../components/Upload"
 
 export function Refund() {
     const [category, setCategory] = useState("")
@@ -30,6 +31,8 @@ export function Refund() {
                 <Input required legend="Valor"/>
             </div>
 
+
+            <Upload />
         </form>
     )
 }
